065f68867b6964d30bbe351315805784850001bf
mengwei <mengwei@thunisedu.com>
2022-09-22 19:33:04 +0800

    updaate

--- TZImagePickerController/TZImagePickerController/TZPhotoPreviewController.h
+++ TZImagePickerController/TZImagePickerController/TZPhotoPreviewController.h
@@ -10,6 +10,8 @@
 @class TZAssetModel;
 @interface TZPhotoPreviewController : UIViewController
 
+@property (nonatomic, strong) NSMutableArray *selectModels;                  ///< 选中的图片集合
+
 @property (nonatomic, strong) NSMutableArray *models;                  ///< All photo models / 所有图片模型数组
 @property (nonatomic, strong) NSMutableArray *photos;                  ///< All photos  / 所有图片数组
 @property (nonatomic, assign) NSInteger currentIndex;           ///< Index of the photo user click / 用户点击的图片的索引
